360fabbe4f2fd23892ec0ef62012c9b044650c01
Jack <Jack@localhost>
2023-10-04 15:33:09 -0500

    Finished Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,21 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		double o = 0 ;
+		double t = 0 ;
+		double halfWidth = 0.5 ;
+		double halfHeight = 0.25 ;
+		
+		
+		
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledRectangle(o, t, halfWidth, halfHeight) ;
+		
+		StdDraw.setPenRadius(0.02);
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		 double[] x = { 0, 0.25, 0.5,};
+		 double[] y = { 0, 0.25, 0};
+		 StdDraw.polygon(x, y);
 		
 	}
 }
